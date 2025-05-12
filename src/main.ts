38e0bed25b4670711ec888503a89d7de083c3000
HieuHoang83 <139992556+HieuHoang83@users.noreply.github.com>
2025-05-12 20:53:19 +0700

    mở public env  + sử dụng database online

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
   });
 
